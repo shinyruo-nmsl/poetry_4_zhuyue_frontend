ea8f82b2a02bac89cf3c8128c96671c40d9f428b
jim <jiminhu@trip.com>
2024-05-21 17:02:31 +0800

    新增角色类型

--- src/service/user.ts
+++ src/service/user.ts
@@ -2,7 +2,7 @@ import { Opiton } from "../global-type/model";
 import { Role } from "../global-type/user";
 import { AuthToken } from "../util/http";
 
-export const UserRoles: Role[] = ["admin", "ordinary"];
+export const UserRoles: Role[] = ["admin", "member", "ordinary"];
 
 export const UserRoleOptions: Opiton<Role>[] = UserRoles.map((role) => ({
   label: formatUserRole(role),
@@ -17,6 +17,8 @@ export function formatUserRole(role: Role) {
   switch (role) {
     case "admin":
       return "管理员";
+    case "member":
+      return "会员";
     case "ordinary":
       return "普通用户";
     default:

2c0dc53056b87ea3da74960b46cf8e08760bb66b
jim <jiminhu@trip.com>
2024-05-22 19:41:16 +0800

    打包兼容旧版本浏览器

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+      apply: "build",
+    },
+  ],
   server: {
     port: 8888,
   },
